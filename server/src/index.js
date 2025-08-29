d97ee3b71596f45a473d95cf6375da2a0fe2c7cd
Guilherme Muniz <gui.munizalemao@gmail.com>
2025-08-28 21:22:45 -0300

    feat(server): inicializa servidor express com pool de conexão mysql

---
+++ server/src/index.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql2/promise");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
